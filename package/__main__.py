c094596eb928843e79d84402e15d7dee52599fa0
Vincent Lin <soulace2425@outlook.com>
2022-09-08 22:48:44 -0700

    Set up logging

--- package/__main__.py
+++ package/__main__.py
@@ -5,7 +5,7 @@ Package entry point.
 
 from .core import get_status
 from .driver import get_driver
-from .errors import send_error_email
+from .logger import log_exit_status, send_error_email
 from .writer import log_status
 
 print("Running the status-logger package...")
@@ -14,7 +14,9 @@ try:
     driver = get_driver()
     emoji, text = get_status(driver)
     log_status(emoji, text)
+    log_exit_status(None)
     print("Finished running the status-logger package: SUCCESS.")
 except Exception as e:
     send_error_email(e)
+    log_exit_status(e)
     print("Finished running the status-logger package: ERROR.")
