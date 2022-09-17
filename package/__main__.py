223b10e4f6bcf5bc8e47c695e66a449581085045
Vincent Lin <soulace2425@outlook.com>
2022-09-16 17:46:35 -0700

    Add simple argparser for debugging options

--- package/__main__.py
+++ package/__main__.py
@@ -4,19 +4,27 @@ Package entry point.
 """
 
 import sys
+from argparse import ArgumentParser
 
 from .core import get_status
 from .driver import get_driver
 from .logger import log_exit_status, send_error_email
 from .writer import log_status
 
-# Don't log to files and send email
-CONSOLE_ONLY = False
+# Debugging options
+parser = ArgumentParser(description="Run the status-logger program")
+parser.add_argument("-c", "--console",
+                    action="store_true",
+                    help="Only output to the console, no logs or email")
+parser.add_argument("-w", "--window",
+                    action="store_true",
+                    help="Run the scraper windowed instead of headlessly")
+ns = parser.parse_args(sys.argv[1:])
 
-# Run the program windowed (debugging) if any args are supplied
-headless = (len(sys.argv) == 1)
+console_only = ns.console
+headless = not ns.window
 
-print(f"Running the status-logger package ({CONSOLE_ONLY=}, {headless=})...")
+print(f"Running the status-logger package ({console_only=}, {headless=})...")
 
 try:
     with get_driver(headless) as driver:
@@ -29,13 +37,13 @@ try:
             "status set up for today, ignore this."
         )
 except Exception as e:
-    if not CONSOLE_ONLY:
+    if not console_only:
         log_exit_status(e)
         send_error_email(e)
     print("Finished running the status-logger package: ERROR.")
     print(f"{type(e).__name__}: {e}")
 else:
-    if not CONSOLE_ONLY:
+    if not console_only:
         log_status(emoji, text)
         log_exit_status(None)
     print("Finished running the status-logger package: SUCCESS.")
