df7bcc757b1945191f433b80456ac45daf97138e
Vincent Lin <vinlin24@outlook.com>
2023-03-16 03:19:44 -0700

    Use webdriver-manager to handle Edge driver stuff

--- package/driver.py
+++ package/driver.py
@@ -3,17 +3,13 @@
 Initializes the web driver instance.
 """
 
-import os
 from collections.abc import Generator
 from contextlib import contextmanager
 
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.edge.service import Service
-
-DRIVER_PATH = os.path.join(os.path.dirname(__file__),
-                           "../bin/msedgedriver.exe")
-"""Absolute path to Edge web driver executable."""
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 WAIT_TIMEOUT = 15.0
 """Time in seconds for driver to wait for pages to load."""
@@ -22,7 +18,7 @@ WAIT_TIMEOUT = 15.0
 @contextmanager
 def get_driver(headless: bool) -> Generator[webdriver.Edge, None, None]:
     """Initialize and return the Edge web driver instance to use."""
-    service = Service(DRIVER_PATH)
+    service = Service(EdgeChromiumDriverManager().install())
     options = Options()
     options.headless = headless
     options.add_argument("--window-size=1920,1080")
