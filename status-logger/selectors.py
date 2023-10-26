65dfa30e064a8274baba9322de2bfc8f31b6e470
Vincent Lin <vinlin24@outlook.com>
2023-10-26 03:32:11 -0700

    Update emoji <img> and text <span> selectors

--- status-logger/selectors.py
+++ status-logger/selectors.py
@@ -3,10 +3,10 @@
 Stores CSS selectors of useful web elements as string constants.
 """
 
-EMOJI_IMG = "#app-mount > div.appAsidePanelWrapper-VeklAl > div.notAppAsidePanel-1yXaPq > div.app-1BipOi > div > div.layers-OrUESM.layers-1YQhyW > div > div > div > div > div > section > div.container-1CH86i > div.avatarWrapper-12uuu4.withTagAsButton-2_JkHX > div.nameTag-2Q56-H.canCopy-rj-Am8 > div.panelSubtextContainer-30etH9 > div > div > div.default-rFvAvS > div > img"
+EMOJI_IMG = "#app-mount > div.appAsidePanelWrapper__714a6 > div.notAppAsidePanel__9d124 > div.app_b1f720 > div > div.layers_a40797.layers__12c9f > div > div > div > div > div > section > div.container_ca50b9 > div.avatarWrapper_ba5175.withTagAsButton_cc125f > div.nameTag__0e320.canCopy__81263 > div.panelSubtextContainer_f28bed > div > div > div.default_cae228 > div > img"
 """The <img> of the emoji part, if included."""
 
-TEXT_SPAN = "#app-mount > div.appAsidePanelWrapper-VeklAl > div.notAppAsidePanel-1yXaPq > div.app-1BipOi > div > div.layers-OrUESM.layers-1YQhyW > div > div > div > div > div > section > div.container-1CH86i > div.avatarWrapper-12uuu4.withTagAsButton-2_JkHX > div.nameTag-2Q56-H.canCopy-rj-Am8 > div.panelSubtextContainer-30etH9 > div > div > div.default-rFvAvS > div > span"
+TEXT_SPAN = "#app-mount > div.appAsidePanelWrapper__714a6 > div.notAppAsidePanel__9d124 > div.app_b1f720 > div > div.layers_a40797.layers__12c9f > div > div > div > div > div > section > div.container_ca50b9 > div.avatarWrapper_ba5175.withTagAsButton_cc125f > div.nameTag__0e320.canCopy__81263 > div.panelSubtextContainer_f28bed > div > div > div.default_cae228 > div > span"
 """The <span> of the text part, if included."""
 
 EMAIL_INPUT = "#uid_5"
